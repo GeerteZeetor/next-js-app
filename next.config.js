f73d551057fe5e8c8b11de892ed23b3f7af8ed9e
AndreyMorozov <pisemest@yandex.ru>
2023-03-07 00:01:56 +0500

    init commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,57 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+/* eslint-disable no-undef */
+// module.exports = {
+//   webpack(config) {
+//     config.module.rules.push({
+//       loader: '@svgr/webpack',
+//       issuer: /\.[jt]sx?$/,
+//       options: {
+//         svgo: true,
+//         svgoConfig: {
+//           plugins: [
+//             {
+//               name: 'preset-default',
+//               params: { override: { removeViewBox: false } },
+//             },
+//           ],
+//         },
+//         titleProp: true,
+//       },
+//       test: /\.svg$/,
+//     });
+//     return config;
+//   },
+// };
 
-module.exports = nextConfig
+module.exports = {
+  // other configs...
+
+  // future: { webpack5: true }, // -- not needed since Next.js v11.0.0
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: { and: [/\.(js|ts|md)x?$/] },
+      use: [
+        {
+          loader: '@svgr/webpack',
+          options: {
+            prettier: false,
+            svgo: true,
+            svgoConfig: {
+              plugins: [
+                {
+                  name: 'preset-default',
+                  params: {
+                    overrides: { removeViewBox: false },
+                  },
+                },
+              ],
+            },
+
+            titleProp: true,
+          },
+        },
+      ],
+    });
+    return config;
+  },
+};
